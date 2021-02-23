1dcf68142a64af3f861054f480e016a2ee2c4733
sedhomm1 <mark_sedhom@DESKTOP-CV4PDRS.localdomain>
2021-02-23 05:13:23 -0500

    surprised but happy?x4

--- docs/api/test.py
+++ docs/api/test.py
@@ -2,13 +2,19 @@ from http.server import BaseHTTPRequestHandler
 from datetime import datetime
 
 class handler(BaseHTTPRequestHandler):
+  def __init__(self):
+    self.name = "kalb"
+  
+
+  def do_POST(self):
+    content_len = int(self.headers.get('Content-Length'))
+    self.name = self.rfile.read(content_len)
+
 
   def do_GET(self):
-    content_len = int(self.headers.get('Content-Length'))
-    name = self.rfile.read(4)
     self.send_response(200)
     self.send_header('Content-type', 'text/plain')
     self.end_headers()
-    string = "Yoooo how's it going " + name + ". U suck!"
+    string = "Yoooo how's it going " + self.name + ". U suck!"
     self.wfile.write(string.encode())
-    return
+    return 
