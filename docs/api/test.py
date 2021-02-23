137c29ac50fdee0357991a63eafc768912561654
sedhomm1 <mark_sedhom@DESKTOP-CV4PDRS.localdomain>
2021-02-23 05:17:55 -0500

    surprised but happy?x7

--- docs/api/test.py
+++ docs/api/test.py
@@ -2,8 +2,8 @@ from http.server import BaseHTTPRequestHandler
 from datetime import datetime
 
 class handler(BaseHTTPRequestHandler):
-  def __init__(self):
-    self.name = "kalb"
+  # def __init__(self):
+  #   self.name = "kalb"
   
 
   # def do_POST(self):
@@ -15,6 +15,7 @@ class handler(BaseHTTPRequestHandler):
     self.send_response(200)
     self.send_header('Content-type', 'text/plain')
     self.end_headers()
-    string = "Yoooo how's it going " + self.name + ". U suck!"
+    name = "kalb"
+    string = "Yoooo how's it going " + name + ". U suck!"
     self.wfile.write(string.encode())
     return 
